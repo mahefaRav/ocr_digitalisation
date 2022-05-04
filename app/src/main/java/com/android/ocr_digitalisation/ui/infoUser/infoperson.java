09632697b1451c28b68731128e5e4aac03404eca
unknown <r.kevinelliot@gmail.com>
2022-05-04 12:35:55 +0300

    infouser

---
+++ app/src/main/java/com/android/ocr_digitalisation/ui/infoUser/infoperson.java
@@ -0,0 +1,16 @@
+package com.android.ocr_digitalisation.ui.infoUser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.ocr_digitalisation.R;
+
+public class infoperson extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infoperson);
+    }
+}

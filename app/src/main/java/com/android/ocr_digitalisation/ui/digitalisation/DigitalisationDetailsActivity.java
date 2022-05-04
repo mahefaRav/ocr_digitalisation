a9e8f8128600d382670ffb7e0d66276a9ce5f096
FreakAfo <46443733+mahefaRav@users.noreply.github.com>
2022-05-04 12:37:01 +0300

    c

--- app/src/main/java/com/android/ocr_digitalisation/ui/digitalisation/DigitalisationDetailsActivity.java
+++ app/src/main/java/com/android/ocr_digitalisation/ui/digitalisation/DigitalisationDetailsActivity.java
@@ -2,9 +2,15 @@ package com.android.ocr_digitalisation.ui.digitalisation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
 
 import com.android.ocr_digitalisation.R;
+import com.android.ocr_digitalisation.ui.login.LoginActivity;
 
 public class DigitalisationDetailsActivity extends AppCompatActivity {
 
@@ -13,4 +19,6 @@ public class DigitalisationDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_digitalisation_details);
     }
+
+
 }
--- app/src/main/java/com/android/ocr_digitalisation/ui/login/LoginActivity.java
+++ app/src/main/java/com/android/ocr_digitalisation/ui/login/LoginActivity.java
@@ -2,9 +2,13 @@ package com.android.ocr_digitalisation.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.android.ocr_digitalisation.R;
+import com.android.ocr_digitalisation.ui.digitalisation.DigitalisationDetailsActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +16,21 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    private void setOnclickListener(){
+        final Button loginButton=findViewById(R.id.login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(LoginActivity.this, DigitalisationDetailsActivity.class);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
